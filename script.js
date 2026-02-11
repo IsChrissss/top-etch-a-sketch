57eb09accf19a6c77fbada38c7ee18b95b567c9d
Ionut Cristian Enoiu <cristianionut2015@gmail.com>
2026-02-11 13:11:11 +0100

    Change the JS file so everytime the user clicks the button, the container clearse the content to give space for the new one. Add comments

--- script.js
+++ script.js
@@ -1,27 +1,54 @@
+//Create the button fot the user
 const btn = document.createElement("button");
 btn.classList.add("sizeBtn");
 btn.textContent = "CLICK HERE!"
 btn.style.cssText = "width: 100px; height: 50px"
 document.body.appendChild(btn);
 
-const container = document.querySelector('.container');
+btn.addEventListener("click", () => {
+    const container = document.querySelector('.container');
+    //It clears the container every time the button is clicked
+    container.innerHTML = ''; 
+    
+    let correctInput = false;
+    let rows = 0;
+    let columns = 0;
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        container.appendChild(square);
+    //Set a while conditional state until the user enters the correct input
+    while (!correctInput) {
+        rows = Number(prompt("Enter the number of Rows (maximum 100): "))
+        columns = rows;
+        if (((rows <= 100) && (rows > 0))) {
+            correctInput = true;
+        } else {
+            alert("Incorrect input. Try again")
+        }
     }
-}
 
-const squares = document.querySelectorAll('.square');
+    //Save rows and columns in Str format
+    btn.dataset.rows = rows;
+    btn.dataset.columns = columns;
+    
+    //Fills the container with square divs
+    for (let i = 0; i < Number(btn.dataset.rows); i++) {
+        for (let j = 0; j < Number(btn.dataset.columns); j++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            square.style.cssText = `flex-basis: calc(100% / ${btn.dataset.columns}); height: calc(100% / ${btn.dataset.rows})`
+            container.appendChild(square);
+        }
+    }
 
-for (const square of squares) {
-    square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`;
+    //Adds an event for when hovering the mouse over squares to change color
+    container.addEventListener('mouseover', (e) => {
+        if (e.target.classList.contains('square')) {
+            e.target.style.backgroundColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`;
+        }
     });
-}
+    
+    //Returns a RGB color number
+    function randomColor() {
+        return Math.floor(Math.random() * 256)
+    }
+})
 
-function randomColor() {
-    return Math.floor(Math.random() * 256)
-}
